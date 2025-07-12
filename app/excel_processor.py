236a6983d488cd17af7e6dd84d3a73a390dae19d
ArmandoSandino <juan.armando.sandino.ba@gmail.com>
2025-07-12 17:41:40 -0600

    fix; avoid empty cells and non-numerical values

--- app/excel_processor.py
+++ app/excel_processor.py
@@ -5,4 +5,8 @@ from io import BytesIO
 # Leer fichero excel y convertirlo en una lista de diccionarios
 def process_excel( file: BytesIO ) -> List[Dict]:
     df = pd.read_excel( file )
+
+    # Reemplazar NaN por None (compatible con JSON/Firebase)
+    df =  df.where( pd.notnull(df), None )
+    
     return df.to_dict( orient="records" )
