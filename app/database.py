dd9ec379b8a0318699c71bb476fd98be9c118e2e
ArmandoSandino <juan.armando.sandino.ba@gmail.com>
2025-07-18 16:12:52 -0600

    refactor chore; add os environment

--- app/database.py
+++ app/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
 # DATABASE_URL = "postgresql+asyncpg://username:password@ep-xxxxx.neon.tech/dbname"
-DATABASE_URL = 'postgresql://neondb_owner:npg_BgrhMKzN89aS@ep-spring-flower-ae6fcmba-pooler.c-2.us-east-2.aws.neon.tech/manage-policy?sslmode=require&channel_binding=require'
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
